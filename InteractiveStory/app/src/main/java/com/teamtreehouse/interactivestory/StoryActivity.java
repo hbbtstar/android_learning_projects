872d5c8227aeb98e85a31b5818525c550cb274c7
Alexander Lourenco <Alexander Lourenco>
2016-03-14 11:11:08 +0100

    initial commit- android learning projects

---
+++ InteractiveStory/app/src/main/java/com/teamtreehouse/interactivestory/StoryActivity.java
@@ -0,0 +1,21 @@
+package com.teamtreehouse.interactivestory;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class StoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
