c26892fe5a3ef79b263d9f1aade0eeaa10d63833
Alexander Lourenco <alexander.lourenco@gmail.com>
2016-03-16 21:40:24 +0100

    actually add loadPage call in storyActivity

--- InteractiveStory/app/src/main/java/com/teamtreehouse/interactivestory/ui/StoryActivity.java
+++ InteractiveStory/app/src/main/java/com/teamtreehouse/interactivestory/ui/StoryActivity.java
@@ -21,6 +21,7 @@ public class StoryActivity extends AppCompatActivity {
     private TextView mTextView;
     private Button mChoice1;
     private Button mChoice2;
+    private String mName;
 
 
     @Override
@@ -29,22 +30,25 @@ public class StoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_story);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
-        if (name == null) {
-            name = "Friend";
+        mName = intent.getStringExtra(getString(R.string.key_name));
+        if (mName == null) {
+            mName = "Friend";
         }
-        Log.d(TAG, name);
+        Log.d(TAG, mName);
         mImageView = (ImageView)findViewById(R.id.storyImageView);
         mTextView = (TextView)findViewById(R.id.storyTextView);
         mChoice1 = (Button)findViewById(R.id.choiceButton1);
         mChoice2 = (Button)findViewById(R.id.choiceButton2);
+        loadPage();
     }
 
     private void loadPage() {
         Page page = mStory.getPage(0);
         Drawable drawable = getResources().getDrawable(page.getImageId());
         mImageView.setImageDrawable(drawable);
-        mTextView.setText(page.getText());
+        String pageText = page.getText();
+        pageText = String.format(pageText, mName);
+        mTextView.setText(pageText);
         mChoice1.setText(page.getChoice1().getText());
         mChoice2.setText(page.getChoice2().getText());
     }
