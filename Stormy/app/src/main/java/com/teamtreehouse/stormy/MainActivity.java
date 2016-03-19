81a5131b141488435c8707f2c4af51594c0e499d
Alexander Lourenco <alexander.lourenco@gmail.com>
2016-03-19 23:41:25 +0100

    added network call

--- Stormy/app/src/main/java/com/teamtreehouse/stormy/MainActivity.java
+++ Stormy/app/src/main/java/com/teamtreehouse/stormy/MainActivity.java
@@ -2,12 +2,48 @@ package com.teamtreehouse.stormy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        String apiKey = "0446c8f5b0be9de182860491c06cb82f";
+        double latitude = 37.8267;
+        double longitude = -122.423;
+        String forecastUrl="https://api.forecast.io/forecast/" + apiKey +
+                "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(forecastUrl)
+                .build();
+
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            if (response.isSuccessful()) {
+                Log.v(TAG, response.body().string());
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Exception caught: ", e);
+        }
     }
 }
+
