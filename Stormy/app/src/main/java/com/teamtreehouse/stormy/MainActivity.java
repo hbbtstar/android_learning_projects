1b2cfbce75ad5c6d03e0c4e64c6f942adfaf8fe3
Alexander Lourenco <alexander.lourenco@gmail.com>
2016-03-20 23:46:24 +0100

    catch network unavailable

--- Stormy/app/src/main/java/com/teamtreehouse/stormy/MainActivity.java
+++ Stormy/app/src/main/java/com/teamtreehouse/stormy/MainActivity.java
@@ -1,12 +1,17 @@
 package com.teamtreehouse.stormy;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -28,22 +33,55 @@ public class MainActivity extends AppCompatActivity {
         double longitude = -122.423;
         String forecastUrl="https://api.forecast.io/forecast/" + apiKey +
                 "/" + latitude + "," + longitude;
+        if (isNetworkAvailable()) {
 
-        OkHttpClient client = new OkHttpClient();
 
-        Request request = new Request.Builder()
-                .url(forecastUrl)
-                .build();
+            OkHttpClient client = new OkHttpClient();
 
-        Call call = client.newCall(request);
-        try {
-            Response response = call.execute();
-            if (response.isSuccessful()) {
-                Log.v(TAG, response.body().string());
-            }
-        } catch (IOException e) {
-            Log.e(TAG, "Exception caught: ", e);
+            Request request = new Request.Builder()
+                    .url(forecastUrl)
+                    .build();
+
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+                }
+            });
         }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        else {
+            Toast.makeText(MainActivity.this, "Network is unavailable!", Toast.LENGTH_SHORT).show();
+        }
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 }
 
